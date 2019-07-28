ee0643128de95acb62b10067dc88a259d9557e15
Andrey <andr1bunk02@gmail.com>
2019-07-28 13:04:20 +0300

    init

---
+++ src/main/java/com/abunko/wardeployment/controller/WebController.java
@@ -0,0 +1,12 @@
+package com.abunko.wardeployment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+	@RequestMapping(value={"/"})
+    public String home(){
+        return "home";
+    }
+}
